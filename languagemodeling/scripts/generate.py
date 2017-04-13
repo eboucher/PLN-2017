37c66020763ede52a886b2cbe83e3e72eeb72f7e
Esteban Boucher <eboucher7@gmail.com>
2017-04-13 20:47:48 -0300

    Getting started with generate.py script

---
+++ languagemodeling/scripts/generate.py
@@ -0,0 +1,12 @@
+"""
+Generate natural language sentences using a language model.
+
+Usage:
+  generate.py -i <file> -n <n>
+  generate.py -h | --help
+
+Options:
+  -i <file>     Language model file.
+  -n <n>        Number of sentences to generate.
+  -h --help     Show this screen.
+"""
