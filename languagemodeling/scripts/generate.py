fb72fdf3191d2b48552f83e263d480d509f14a6e
Esteban Boucher <eboucher7@gmail.com>
2017-04-13 21:42:52 -0300

    generate.py script written but not working due to an UnpicklingError

--- languagemodeling/scripts/generate.py
+++ languagemodeling/scripts/generate.py
@@ -10,3 +10,22 @@ Options:
   -n <n>        Number of sentences to generate.
   -h --help     Show this screen.
 """
+from docopt import docopt
+import pickle
+
+from languagemodeling.ngram import NGramGenerator
+
+if __name__ == '__main__':
+    opts = docopt(__doc__)
+
+    filename = opts['-i']
+
+    train_set = open(filename, 'rb')
+    ngram_gen = pickle.load(train_set)
+    input_file.close()
+
+    no_sents = int(opts['-n'])
+
+    for i in range(no_sents):
+        sent = ngram_gen.generate_sent()
+        print(sent + ' ')
